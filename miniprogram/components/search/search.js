9350aad2f51b690520d84089591e5bcfd0cfd8df
m01i0ng <alley.ma@qq.com>
2020-06-24 23:57:18 +0800

    添加 search 组件

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,23 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder: {
+      type: String,
+      value: '搜索',
+    },
+  },
+  externalClasses: ['iconfont', 'icon-search'],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
